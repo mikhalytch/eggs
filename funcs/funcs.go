a73b8aec378d9cf3cb8e1991d6ef1dda2d0288ad
Aleksey Mikhaylov <gmikhalytch@gmail.com>
2022-12-12 01:53:45 +0600

    rm method

--- funcs/funcs.go
+++ funcs/funcs.go
@@ -1,15 +1,11 @@
 package funcs
 
 type (
-	Method[T any] func(t T)
-
 	Applier[T any, R any] func(T) R
 
 	Predicate[T any]        Applier[T, bool]
 	Mapper[A any, B any]    Applier[A, B]
 	OptMapper[A any, B any] Applier[A, *B]
 )
-
-func (f Method[T]) Apply(t T) { f(t) }
 
 func (f Applier[T, R]) Apply(t T) R { return f(t) }
