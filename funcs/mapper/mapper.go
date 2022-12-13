399a96aba51ffee5c2e102c36abbdff1189ecab8
Aleksey Mikhaylov <gmikhalytch@gmail.com>
2022-12-13 06:07:12 +0600

    mapper.Always

--- funcs/mapper/mapper.go
+++ funcs/mapper/mapper.go
@@ -1,4 +1,9 @@
 package mapper
 
-func String[A ~string](a A) string { return string(a) }
-func Identity[A any](a A) A        { return a }
+import (
+	"github.com/mikhalytch/eggs/funcs"
+)
+
+func String[A ~string](a A) string         { return string(a) }
+func Identity[A any](a A) A                { return a }
+func Always[A any](a A) funcs.Mapper[A, A] { return func(_ A) A { return a } }
--- funcs/mapper/mapper_test.go
+++ funcs/mapper/mapper_test.go
@@ -21,3 +21,11 @@ func TestIdentity(t *testing.T) {
 	require.Equal(t, "a", mapper.Identity("a"))
 	require.Equal(t, 1, mapper.Identity(1))
 }
+
+func TestAlways(t *testing.T) {
+	alw := mapper.Always(5)
+
+	require.Equal(t, 5, alw(-100))
+	require.Equal(t, 5, alw(0))
+	require.Equal(t, 5, alw(1))
+}
