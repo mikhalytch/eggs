4c6a791df2e76826486a306a8d98b3b6d18b9654
Aleksey Mikhaylov <gmikhalytch@gmail.com>
2022-12-13 13:46:15 +0600

    eitherr -> try

---
+++ try/try.go
@@ -0,0 +1,97 @@
+package try
+
+import (
+	"github.com/mikhalytch/eggs/funcs"
+	"github.com/mikhalytch/eggs/opt"
+)
+
+type (
+	// Try is like Either, with left type simplified to error
+	// (Basically, Try[T].ToEither() :: Either[error, T]).
+	Try[R any] interface {
+		// OrElse returns contained value if this is success; otherwise returns given `other` value.
+		OrElse(other R) R
+
+		// ForEach executes given procedure for success, skips for failure.
+		ForEach(procedure funcs.Procedure[R]) Try[R]
+
+		// ToErr is nil for success, and error for failure
+		ToErr() error
+
+		// ToOpt converts success to opt.Some, failure to opt.None.
+		ToOpt() opt.Option[R]
+
+		// Map is category unchanging method, variant of Map function.
+		Map(mapper funcs.Mapper[R, R]) Try[R]
+		MapFailure(lm funcs.Mapper[error, error]) Try[R]
+
+		// FlatMap is category unchanging method, variant of FlatMap function.
+		FlatMap(fMap FMapper[R, R]) Try[R]
+	}
+	// success represents the success value of Try.
+	success[R any] struct{ r R }
+	// failure represents failure value of Try.
+	failure[R any] struct{ err error }
+
+	FMapper[R, V any] funcs.Mapper[R, Try[V]]
+)
+
+// ----- success -----
+
+func (r success[R]) OrElse(_ R) R                                   { return r.r }
+func (r success[R]) ToErr() error                                   { return nil }
+func (r success[R]) ToOpt() opt.Option[R]                           { return opt.Some(r.r) }
+func (r success[R]) Map(m funcs.Mapper[R, R]) Try[R]                { return Map[R, R](r, m) }
+func (r success[R]) MapFailure(_ funcs.Mapper[error, error]) Try[R] { return r }
+func (r success[R]) FlatMap(fMap FMapper[R, R]) Try[R]              { return FlatMap[R, R](r, fMap) }
+func (r success[R]) ForEach(p funcs.Procedure[R]) Try[R] {
+	p(r.r)
+
+	return r
+}
+
+// ----- failure -----
+
+func (l failure[R]) OrElse(other R) R                                { return other }
+func (l failure[R]) ToErr() error                                    { return l.err }
+func (l failure[R]) ToOpt() opt.Option[R]                            { return opt.None[R]() }
+func (l failure[R]) Map(m funcs.Mapper[R, R]) Try[R]                 { return Map[R, R](l, m) }
+func (l failure[R]) MapFailure(lm funcs.Mapper[error, error]) Try[R] { return Failure[R](lm(l.err)) }
+func (l failure[R]) FlatMap(fMap FMapper[R, R]) Try[R]               { return FlatMap[R, R](l, fMap) }
+func (l failure[R]) ForEach(_ funcs.Procedure[R]) Try[R]             { return l }
+
+// ----- General -----
+
+func Success[R any](r R) Try[R]       { return success[R]{r: r} }
+func Failure[R any](err error) Try[R] { return failure[R]{err: err} }
+func Trie[R any](r R, err error) Try[R] {
+	if err != nil {
+		return Failure[R](err)
+	}
+
+	return Success[R](r)
+}
+
+func LiftOption[R any](o opt.Option[R], err error) Try[R] {
+	/*Trie(o.Get()).MapFailure(mapper.Always(err))*/
+	return opt.Fold[R, Try[R]](o, Success[R], func() Try[R] { return Failure[R](err) })
+}
+
+// Map returns success containing the result of applying f to m if it's not failure;
+// otherwise it returns unchanged failure.
+func Map[R, V any](e Try[R], f funcs.Mapper[R, V]) Try[V] {
+	if s, ok := e.(success[R]); ok {
+		return success[V]{r: f(s.r)}
+	}
+
+	return Failure[V](e.ToErr())
+}
+
+// FlatMap returns success with result of applying f to R, if this is success; otherwise it returns unchanged failure.
+func FlatMap[R, V any](e Try[R], f FMapper[R, V]) Try[V] {
+	if s, ok := e.(success[R]); ok {
+		return f(s.r)
+	}
+
+	return Failure[V](e.ToErr())
+}
---
+++ try/try_test.go
@@ -0,0 +1,118 @@
+package try_test
+
+import (
+	"errors"
+	"io"
+	"net/http"
+	"strconv"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/mikhalytch/eggs/funcs/mapper"
+	"github.com/mikhalytch/eggs/opt"
+	"github.com/mikhalytch/eggs/try"
+)
+
+func TestFailure_OrElse(t *testing.T) {
+	require.Equal(t, 2, try.LiftOption(opt.None[int](), io.EOF).OrElse(2))
+	require.Equal(t, "abc", try.Failure[string](io.EOF).OrElse("abc"))
+}
+
+func TestSuccess_OrElse(t *testing.T) {
+	require.Equal(t, 42, try.Success[int](42).OrElse(10))
+}
+
+func TestForEach(t *testing.T) {
+	cnt := 0
+	inc := func(i any) { cnt++ }
+
+	try.Failure[any](io.EOF).ForEach(inc)
+	require.Equal(t, 0, cnt)
+
+	try.Success[any]("10").ForEach(inc)
+	require.Equal(t, 1, cnt)
+}
+
+func TestToErr(t *testing.T) {
+	require.NoError(t, try.Success(10).ToErr())
+	require.Equal(t, io.EOF, try.Failure[any](io.EOF).ToErr())
+}
+
+func TestToOpt(t *testing.T) {
+	require.NotEqual(t, opt.None[int](), try.Failure[any](io.EOF).ToOpt())
+	require.EqualValues(t, opt.None[int](), try.Failure[any](io.EOF).ToOpt())
+	require.Equal(t, opt.None[int](), try.Failure[int](io.EOF).ToOpt())
+	require.Equal(t, opt.Some(10), try.Success(10).ToOpt())
+}
+
+func TestFailure_Map(t *testing.T) {
+	require.Equal(t, try.Failure[int](io.EOF), try.Failure[int](io.EOF).Map(mapper.Identity[int]))
+	require.Equal(t, try.Failure[int](io.EOF), try.Failure[int](io.EOF).Map(func(i int) int { return i * 1000 }))
+}
+
+func TestSuccess_Map(t *testing.T) {
+	require.Equal(t, try.Success(10), try.Success(10).Map(mapper.Identity[int]))
+	require.Equal(t, try.Success(10), try.Success(1).Map(func(i int) int { return i * 10 }))
+}
+
+func TestMap(t *testing.T) {
+	cnt := 0
+	fallible := func() (int, error) {
+		cnt++
+		if cnt%2 == 0 {
+			return 0, io.EOF
+		}
+
+		return cnt, nil
+	}
+	require.Equal(t, try.Success(1), try.Map(try.Trie(fallible()), mapper.Identity[int]))
+	require.Equal(t, try.Failure[int](io.EOF), try.Map(try.Trie(fallible()), mapper.Identity[int]))
+	require.Equal(t, try.Success(30),
+		try.Map(try.Trie(fallible()), func(i int) int { return i * 10 }))
+	require.Equal(t, try.Failure[int](io.EOF),
+		try.Map(try.Trie(fallible()), func(i int) int { return i * 10 }))
+}
+
+func TestMapFailure(t *testing.T) {
+	alw := mapper.Always(http.ErrMissingFile)
+
+	require.Equal(t, try.Success(1), try.Success(1).MapFailure(alw))
+	require.Equal(t, try.Failure[int](http.ErrMissingFile), try.Failure[int](io.EOF).MapFailure(alw))
+
+	cond := func(e1 error) error {
+		if errors.Is(e1, io.EOF) {
+			return http.ErrAbortHandler
+		}
+
+		return http.ErrBodyNotAllowed
+	}
+	require.Equal(t, try.Failure[int](http.ErrAbortHandler), try.Failure[int](io.EOF).MapFailure(cond))
+	require.Equal(t, try.Failure[int](http.ErrBodyNotAllowed), try.Failure[int](http.ErrMissingFile).MapFailure(cond))
+}
+
+func TestFailure_FlatMap(t *testing.T) {
+	createFailureInt := func(_ int) try.Try[int] { return try.Failure[int](http.ErrAbortHandler) }
+
+	require.Equal(t, try.Failure[int](io.EOF), try.Failure[int](io.EOF).FlatMap(try.Success[int]))
+	require.Equal(t, try.Failure[int](io.EOF), try.Failure[int](io.EOF).FlatMap(createFailureInt))
+}
+
+func TestSuccess_FlatMap(t *testing.T) {
+	createFailureInt := func(_ int) try.Try[int] { return try.Failure[int](http.ErrAbortHandler) }
+
+	require.Equal(t, try.Success(1), try.Success[int](1).FlatMap(try.Success[int]))
+	require.Equal(t, try.Failure[int](http.ErrAbortHandler), try.Success[int](1).FlatMap(createFailureInt))
+}
+
+func TestFlatMap(t *testing.T) {
+	createSuccessInt := func(s string) try.Try[int] { return try.Trie(strconv.Atoi(s)) }
+	createFailureString := func(_ int) try.Try[string] { return try.Failure[string](http.ErrAbortHandler) }
+
+	require.Equal(t, try.Success(1), try.FlatMap(try.Success[string]("1"), createSuccessInt))
+	require.Equal(t, try.Failure[int](io.EOF), try.FlatMap(try.Failure[string](io.EOF), createSuccessInt))
+
+	require.Equal(t, try.Failure[string](http.ErrAbortHandler),
+		try.FlatMap(try.Success[int](1), createFailureString))
+	require.Equal(t, try.Failure[int](io.EOF), try.FlatMap(try.Failure[string](io.EOF), createSuccessInt))
+}
