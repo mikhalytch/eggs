34b9afa267915510430e3bf8fe5bc63d2efb729f
Aleksey Mikhaylov <gmikhalytch@gmail.com>
2023-11-23 02:13:01 +0300

    use signed

--- constraints/safecast/types.go
+++ constraints/safecast/types.go
@@ -4,7 +4,7 @@ import baseConstraints "github.com/mikhalytch/eggs/constraints/base"
 
 type (
 	ToInt64 interface {
-		baseConstraints.Integer |
+		baseConstraints.Signed |
 			baseConstraints.Uint | baseConstraints.Uint8 | baseConstraints.Uint16 | baseConstraints.Uint32
 	}
 )
